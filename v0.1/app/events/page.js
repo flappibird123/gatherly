3a957ec5c3d20ee8d7fb4ae1e3b4be5652793ea3
flappibird123 <jasonlilb2@gmail.com>
2025-11-23 15:05:05 +1100

    Add files via upload

---
+++ v0.1/app/events/page.js
@@ -0,0 +1,19 @@
+import EventCard from '../../components/EventCard'
+import { events } from './listings'
+
+export default function Events() {
+    return (
+        <div>
+            <h2 className="text-5xl font-bold text-center tracking-tight text-gray-800 py-6">
+                Events
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                {events.map(event => (
+                    <div key={event.id} className="h-full">
+                        <EventCard event={event} className="h-full" />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
